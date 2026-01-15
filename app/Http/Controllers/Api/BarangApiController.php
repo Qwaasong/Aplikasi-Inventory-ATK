2abc97496f3ef8b22fb0c8d0300d9d2cd182df80
workdewa123 <workdewa123@gmail.com>
2026-01-15 11:13:59 +0700

    memindah controller barang pada api

--- app/Http/Controllers/Api/BarangApiController.php
+++ app/Http/Controllers/Api/BarangApiController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Barang;
+use App\Models\Kategori;
 use Illuminate\Http\Request;
 
 class BarangApiController extends Controller
@@ -27,4 +28,37 @@ class BarangApiController extends Controller
         }
         return response()->json(['success' => false, 'message' => 'Data tidak ditemukan'], 404);
     }
+        public function create()
+    {
+        $kategori = Kategori::all();
+        return view('barang.create', compact('kategori'));
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'nama_barang' => 'required',
+            'id_kategori' => 'required',
+            'jumlah_pack' => 'required|numeric',
+            'jumlah_pcs'  => 'required|numeric',
+        ]);
+
+        Barang::create($request->all());
+        return redirect()->route('barang.index')->with('success', 'Barang berhasil ditambah');
+    }
+
+    public function edit($id)
+    {
+        $barang = Barang::findOrFail($id);
+        $kategori = Kategori::all();
+        return view('barang.edit', compact('barang', 'kategori'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $barang = Barang::findOrFail($id);
+        $barang->update($request->all());
+        return redirect()->route('barang.index')->with('success', 'Barang berhasil diupdate');
+    }
+
 }
